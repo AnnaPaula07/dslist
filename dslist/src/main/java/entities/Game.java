03df4adc7062781f0248878dc1e487006e3e892f
AnnaPaula07 <126425985+AnnaPaula07@users.noreply.github.com>
2024-11-12 16:53:14 -0300

    Adiciona a entidade Game

---
+++ dslist/src/main/java/entities/Game.java
@@ -0,0 +1,27 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="tb_game")
+
+public class Game {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
